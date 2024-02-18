39e090dd2f04fc603fa0918016852a13c06c3697
ZhiyuanJiao <zkjiao73@gmail.com>
2024-02-17 22:04:01 -0800

    Create 78 Subsets.py

---
+++ 78 Subsets.py
@@ -0,0 +1,6 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = [[]]
+        for n in nums:
+            res += [i + [n] for i in res]
+        return res
