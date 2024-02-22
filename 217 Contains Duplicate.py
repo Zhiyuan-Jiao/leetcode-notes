4fe98540f153651c3883022953206035650a0da4
ZhiyuanJiao <zkjiao73@gmail.com>
2024-02-21 20:55:07 -0800

    Create 217 Contains Duplicate.py

---
+++ 217 Contains Duplicate.py
@@ -0,0 +1,10 @@
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        count = set()
+        for i in nums:
+            if i in count: return True
+            count.add(i)
+        return False
+
+# Time complexity: O(n)
+# Space complexity: O(n)
