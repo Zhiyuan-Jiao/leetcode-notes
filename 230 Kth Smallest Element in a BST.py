cb31f0d5f4363ff60e26a119011908cbde458754
ZhiyuanJiao <zkjiao73@gmail.com>
2024-03-11 22:14:08 -0700

    Update 230 Kth Smallest Element in a BST.py

--- 230 Kth Smallest Element in a BST.py
+++ 230 Kth Smallest Element in a BST.py
@@ -19,3 +19,30 @@ class Solution:
 
 # Time complexity: O(n)
 # Space complexity: O(n)
+
+
+# Use iteration with stack to traverse the binary tree!!!!!!!
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        stack = []
+        cur = root
+
+        while stack or cur:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            k -= 1
+            if k == 0: return cur.val
+            cur = cur.right
+
+# Time complexity: O(k)
+# Space complexity: O(logk) for balance binary tree
