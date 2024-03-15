59de286ff4afb688e19f36eb165f79fc09d1bb96
ZhiyuanJiao <zkjiao73@gmail.com>
2024-03-15 12:02:20 -0700

    Update Heap.py

--- Algorithm Templates/Heap.py
+++ Algorithm Templates/Heap.py
@@ -1,14 +1,15 @@
 n = 10
-maxHeap = [i for i in range(n)]
-minHeap = [-i for i in range(n)]
+maxHeap = [-i for i in range(n)]
+minHeap = [i for i in range(n)]
+# time complexity: O(n)
 heapq.heapify(maxHeap)
 heapq.heapify(minHeap)
 
 # time complexity: O(logn), n = 10
-max = heapq.heappop(maxHeap)
+max = -heapq.heappop(maxHeap)
 min = heapq.heappop(minHeap)
 
 v = 10
 # time complexity: O(logn), n = 10
-heapq.heappush(maxHeap, v)
+heapq.heappush(maxHeap, -v)
 heapq.heappush(minHeap, v)
