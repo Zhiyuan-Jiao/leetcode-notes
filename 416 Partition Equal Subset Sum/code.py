a2f8c7814bec3c8869c9081481a8201582538bdd
ZhiyuanJiao <zkjiao73@gmail.com>
2024-04-03 12:18:52 -0700

    Update code.py

--- 416 Partition Equal Subset Sum/code.py
+++ 416 Partition Equal Subset Sum/code.py
@@ -1,15 +1,29 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        if not nums: return False
-        target = sum(nums) / 2
-        sumSet = set()
-        sumSet.add(0)
-        for i in nums:
-            ns = []
-            for s in sumSet:
-                if i + s == target: return True
-                ns.append(i + s)
-            for s in ns:
-                sumSet.add(s)
-        return False
+        if len(nums) == 1: return False
+
+        # def dfs(cur, i):
+        #     # base case
+        #     if sum(cur) > sum(nums) / 2 or i == len(nums): return False
+
+        #     if sum(cur) == sum(nums) / 2: return True
+
+        #     cur.append(nums[i])
+        #     if dfs(cur, i + 1): return True
+        #     cur.pop()
+        #     return dfs(cur, i + 1)
+
+        # return dfs([], 0)
+
+
+        dpSet = set([0])
+        for num in nums:
+            newSet = dpSet.copy()
+            for dpN in dpSet:
+                newSet.add(dpN + num)
+                dpSet = newSet
+        return sum(nums) / 2 in dpSet
+
+# Time complexity: O(n * sumN)
+# Space complexity: O(sumN)
             
