9f3a6bf35709b8f10236ed65919e9787ff8369eb
ZhiyuanJiao <zkjiao73@gmail.com>
2024-04-22 00:33:39 -0700

    Create 205 Isomorphic String.py

---
+++ 205 Isomorphic String.py
@@ -0,0 +1,17 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        # O(n^2)
+        # for i in range(len(s)):
+        #     for j in range(i + 1, len(s)):
+        #         if s[i] == s[j] and t[i] != t[j] or t[i] == t[j] and s[i] != s[j]:
+        #             return False
+        # return True
+    
+        # O(n)
+        sc, tc = defaultdict(list), defaultdict(list)
+        for i in range(len(s)):
+            sc[s[i]].append(i)
+            tc[t[i]].append(i)
+        for j in sc.values():
+            if j not in tc.values(): return False
+        return True
