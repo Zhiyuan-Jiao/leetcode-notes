6defbcf4a8b627a60832722b6317ea2daacbf4d2
ZhiyuanJiao <zkjiao73@gmail.com>
2024-05-09 23:30:11 -0700

    Create 71 Simplify Path.py

---
+++ 71 Simplify Path.py
@@ -0,0 +1,15 @@
+class Solution:
+    def simplifyPath(self, path: str) -> str:
+        stack = []
+        for portion in path.split("/"):
+            if portion == "." or not portion:
+                continue
+            elif portion == "..":
+                if stack:
+                    stack.pop()
+            else:
+                stack.append(portion)
+        return "/" + "/".join(stack)
+
+# Time complexity: O(n)
+# Space complexity: O(n)
