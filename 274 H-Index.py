acb5de1ff9fcda5b20441c509d4225ccd8c31838
ZhiyuanJiao <zkjiao73@gmail.com>
2024-07-08 23:48:11 -0700

    Create 274 H-Index.py

---
+++ 274 H-Index.py
@@ -0,0 +1,12 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations = sorted(citations)
+        for i, c in enumerate(citations):
+            count = len(citations) - i
+            if c >= count: return count
+        return 0
+
+# Note: Iterrate through sorted list to check how many qualified papers left, 
+# return qualified papers when it is smaller or equal to the cur paper citations
+
+# T: O(n) S: O(1)
