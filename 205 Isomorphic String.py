cc3a32336214c4ddfbf6368565883cd8ff13520c
ZhiyuanJiao <zkjiao73@gmail.com>
2024-11-10 11:46:38 -0600

    Update 205 Isomorphic String.py

--- 205 Isomorphic String.py
+++ 205 Isomorphic String.py
@@ -8,10 +8,21 @@ class Solution:
         # return True
     
         # O(n)
-        sc, tc = defaultdict(list), defaultdict(list)
+        # sc, tc = defaultdict(list), defaultdict(list)
+        # for i in range(len(s)):
+        #     sc[s[i]].append(i)
+        #     tc[t[i]].append(i)
+        # for j in sc.values():
+        #     if j not in tc.values(): return False
+        # return True
+    
+        cMap = dict()
+        used = set()
         for i in range(len(s)):
-            sc[s[i]].append(i)
-            tc[t[i]].append(i)
-        for j in sc.values():
-            if j not in tc.values(): return False
+            if s[i] in cMap:
+                if cMap[s[i]] != t[i]: return False
+            else:
+                if t[i] in used: return False
+                cMap[s[i]] = t[i]
+                used.add(t[i])
         return True
