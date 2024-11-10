3de30d2f7858ba90313cc35aec14edc9c3d2bf58
ZhiyuanJiao <zkjiao73@gmail.com>
2024-11-10 13:59:42 -0600

    Update 219 Contains Duplicate II.py

--- 219 Contains Duplicate II.py
+++ 219 Contains Duplicate II.py
@@ -1,11 +1,29 @@
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        hashMap = dict()
+        # q = collections.deque()
+        # for n in nums:
+        #     if n in q:
+        #         return True
+        #     q.append(n)
+        #     if len(q) > k:
+        #         q.popleft()
+        # return False
+        
+        # pastNum = dict()
+        # for i, n in enumerate(nums):
+        #     if n in pastNum and i - pastNum[n] <= k:
+        #         return True
+        #     pastNum[n] = i
+        # return False
+
+        if not k: return False
+        pastK = set()
         for i, n in enumerate(nums):
-            if n in hashMap and i - hashMap[n] <= k:
+            if n in pastK:
                 return True
-            hashMap[n] = i
+            if len(pastK) == k:
+                pastK.remove(nums[i - k])
+            pastK.add(n)
         return False
 
-# Time complexity: O(n)
-# Space complexity: O(n)
+        T: O(n) S: O(k)
