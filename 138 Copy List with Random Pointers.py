fcd53bb4ec55991be15a6c53a803b857d2ccf6a7
ZhiyuanJiao <zkjiao73@gmail.com>
2024-11-13 10:52:28 -0600

    Update 138 Copy List with Random Pointers.py

--- 138 Copy List with Random Pointers.py
+++ 138 Copy List with Random Pointers.py
@@ -26,3 +26,31 @@ class Solution:
 # Note: use a dict to map old node to new node
 # Time complexity: O(n)
 # Space complexity: O(n)
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head: return 
+        cur = head
+        while cur:
+            next = cur.next
+            cur.next = Node(cur.val, next)
+            cur = cur.next.next if cur.next else None
+
+        
+        oldCur, newCur = head, head.next
+        while oldCur:
+            newCur.random = oldCur.random.next if oldCur.random else None
+            oldCur, newCur = oldCur.next.next, newCur.next.next if newCur.next else None
+
+        oldHead, newHead = head, head.next
+        oldCur, newCur = oldHead, newHead
+        while oldCur and newCur:
+            oldCur.next = newCur.next
+            oldCur = oldCur.next
+            newCur.next = oldCur.next if oldCur else None
+            newCur = newCur.next
+        return newHead
+
+# Note: copy the node and insert it after the original node
+# Time complexity: O(n)
+# Space complexity: O(1)
