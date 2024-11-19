66a2649d9ec68258dcdd77de6f4cd9f91bb212a1
ZhiyuanJiao <zkjiao73@gmail.com>
2024-11-19 12:18:22 -0600

    Update 114.Flatten Binary Tree to Linked List.py

--- 114.Flatten Binary Tree to Linked List.py
+++ 114.Flatten Binary Tree to Linked List.py
@@ -27,3 +27,33 @@ class Solution:
 
 # T: O(n) S: O(h)
 # Note: Iterative Binary Tree Preorder Traversal
+
+
+class Solution:
+    def flatten(self, root: Optional[TreeNode]) -> None:
+        """
+        Do not return anything, modify root in-place instead.
+        """
+        if not root: return
+        cur = root
+        while cur:
+            if cur.left: 
+                last = cur.left
+                while last.right:
+                    last = last.right
+                last.right = cur.right
+                cur.right = cur.left
+                cur.left = None
+            cur = cur.right
+
+# T: O(n) S: O(1)
+# Note: continously append the right side of the tree to the right most node in the left side
+#     1
+#    /
+#   2   
+#  / \   
+# 3   4 
+#      \
+#       5
+#        \
+#         6  
